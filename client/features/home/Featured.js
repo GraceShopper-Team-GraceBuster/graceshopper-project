645813a770ef7d9fc4515a526924d7858731c655
NoelDiaz1 <Noeldr183@gmail.com>
2023-04-19 17:28:39 -0400

    successfully created a SingleMovie component and made it so when the user clicks a movie they are redirected to the single movie component

--- client/features/home/Featured.js
+++ client/features/home/Featured.js
@@ -1,36 +1,39 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { NavLink } from 'react-router-dom'
 
 const Featured = ({ movieIds }) => {
-  const [movies, setMovies] = useState([]);
+    const [movies, setMovies] = useState([])
 
-  useEffect(() => {
-    const fetchFeatured = async () => {
-      try {
-        const fetchedMovies = [];
-        for (const id of movieIds) {
-          const { data } = await axios.get(`/api/movies/${id}`);
-          fetchedMovies.push(data);
+    useEffect(() => {
+        const fetchFeatured = async () => {
+            try {
+                const fetchedMovies = []
+                for (const id of movieIds) {
+                    const { data } = await axios.get(`/api/movies/${id}`)
+                    fetchedMovies.push(data)
+                }
+                setMovies(fetchedMovies)
+            } catch (error) {
+                next(error)
+            }
         }
-        setMovies(fetchedMovies);
-      } catch (error) {
-        next(error);
-      }
-    };
-    fetchFeatured();
-  }, [movieIds]);
+        fetchFeatured()
+    }, [movieIds])
 
-  return (
-    <div className="movie-container">
-      {movies.map((movie) => (
-        <div key={movie.id} className="movies">
-          <img src={movie.ImageUrl} />
-          <h2>{movie.Title}</h2>
-          <h3>Price: ${movie.Price}</h3>
+    return (
+        <div className="movie-container">
+            {movies.map((movie) => (
+                <div key={movie.id} className="movies">
+                    <NavLink to={`/movies/${movie.id}`}>
+                        <img src={movie.ImageUrl} />
+                        <h2>{movie.Title}</h2>
+                        <h3>Price: ${movie.Price}</h3>
+                    </NavLink>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
-};
+    )
+}
 
-export default Featured;
+export default Featured
