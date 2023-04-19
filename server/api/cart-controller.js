450083fa54bc882e0e90b1f3c0e334efe6697c62
Justin Cappellini <cappellinij@gmail.com>
2023-04-20 02:55:32 +0800

    fixed location of cart files+changed cart code

--- server/api/cart-controller.js
+++ server/api/cart-controller.js
@@ -4,21 +4,22 @@ const cart = new ShoppingCart();
 
 function addToCart(item) {
   cart.addItem(item);
-  // ... code to update cart view ...
+  // Update the cart view with the new item
 }
 
 function removeFromCart(itemId) {
   cart.removeItem(itemId);
-  // ... code to update cart view ...
+  // Update the cart view to remove the item
 }
 
 function updateCartQuantity(itemId, quantity) {
   cart.updateItemQuantity(itemId, quantity);
-  // ... code to update cart view ...
+  // Update the cart view to reflect the new quantity
 }
 
-// add other functions as needed
+// Define other functions as needed and update the cart view as necessary
 // ...
 
 export { addToCart, removeFromCart, updateCartQuantity };
+
 
