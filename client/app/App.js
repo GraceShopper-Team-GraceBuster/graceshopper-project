1deae32942539a542d68e69418c78bcbd2eae765
Richard <Richman962@gmail.com>
2023-04-19 11:30:06 -0400

    Tweaked Navbar

--- client/app/App.js
+++ client/app/App.js
@@ -1,16 +1,15 @@
-import React from 'react'
-import Navbar from '../features/navbar/Navbar'
-import AppRoutes from './AppRoutes'
-import Movies from '../features/movies/Movies'
+import React from "react";
+import Navbar from "../features/navbar/Navbar";
+import AppRoutes from "./AppRoutes";
+import Movies from "../features/movies/Movies";
 
 const App = () => {
-    return (
-        <div>
-            <Navbar />
-            <AppRoutes />
-            <Movies />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Navbar />
+      <AppRoutes />
+    </div>
+  );
+};
 
-export default App
+export default App;
