990680c45dfefb32c973cd69fb55c08f58ab3daf
Richard <Richman962@gmail.com>
2023-04-21 15:52:34 -0400

    Got a rudimentary cart page working

--- client/app/store.js
+++ client/app/store.js
@@ -1,19 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit'
-import logger from 'redux-logger'
-import authReducer from '../features/auth/authSlice'
-import movieSlice from './movieSlice'
-import userSlice from './userSlice'
-import singleMovieSlice from './singleMovieSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import authReducer from "../features/auth/authSlice";
+import movieSlice from "./movieSlice";
+import userSlice from "./userSlice";
+import singleMovieSlice from "./singleMovieSlice";
+import cartSlice from "./cartSlice";
 
 const store = configureStore({
-    reducer: {
-        auth: authReducer,
-        movies: movieSlice,
-        users: userSlice,
-        singleMovie: singleMovieSlice,
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
-})
+  reducer: {
+    auth: authReducer,
+    movies: movieSlice,
+    users: userSlice,
+    singleMovie: singleMovieSlice,
+    cart: cartSlice,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
 
-export default store
-export * from '../features/auth/authSlice'
+export default store;
+export * from "../features/auth/authSlice";
--- client/features/cart/Cart.js
+++ client/features/cart/Cart.js
@@ -1,15 +1,49 @@
-import React from 'react'
-import Footer from '../footer/Footer';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Footer from "../footer/Footer";
+import { fetchCartItems } from "../../app/cartSlice";
 
 function Cart() {
-    return (
-        <>
-            <div>Cart</div>
-            <footer>
-                <Footer/>
-            </footer>
-        </>
-    )
+  const dispatch = useDispatch();
+  const cartItems = useSelector((state) => state.cart);
+  const userId = useSelector((state) => state.auth.me.id);
+  useEffect(() => {
+    dispatch(fetchCartItems(userId));
+  }, [dispatch, userId]);
+
+  return (
+    <>
+      <h1>Cart</h1>
+      {cartItems.length === 0 ? (
+        <div>Your cart is empty</div>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Quantity</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cartItems.map(
+              (item) =>
+                item.Movie && (
+                  <tr key={item.id}>
+                    <td>{item.Movie.Title}</td>
+                    <td>{item.quantity}</td>
+                    <td>${item.Movie.Price}</td>
+                  </tr>
+                )
+            )}
+          </tbody>
+        </table>
+      )}
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  );
 }
 
-export default Cart; 
+export default Cart;
