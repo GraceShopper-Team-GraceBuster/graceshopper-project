33c69fb47c7aa2ee08974237d7b0040eef65d128
Richard <Richman962@gmail.com>
2023-04-21 14:37:05 -0400

    Add to cart for users working on backend

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -1,57 +1,57 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { Link, useNavigate } from 'react-router-dom'
-import { logout } from '../../app/store'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { logout } from "../../app/store";
 
 const Navbar = () => {
-    const isLoggedIn = useSelector((state) => !!state.auth.me.id)
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    const logoutAndRedirectHome = () => {
-        dispatch(logout())
-        navigate('/login')
-    }
+  const isLoggedIn = useSelector((state) => !!state.auth.me.id);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const logoutAndRedirectHome = () => {
+    dispatch(logout());
+    navigate("/login");
+  };
 
-    return (
-        <div>
-            <nav className="navbar">
-                {isLoggedIn ? (
-                    <div className="navbar-container">
-                        <Link to="/home">
-                            <img src="gracebuster-logo.png" className="logo" />
-                        </Link>
-                        <Link to="/shop">
-                            Shop <i className="fa-solid fa-cassette-tape"></i>
-                        </Link>
-                        <Link to="/account">Account</Link>
-                        <button type="button" onClick={logoutAndRedirectHome}>
-                            Logout
-                        </button>
-                        <Link to="/cart">
-                                <i className="fa-solid fa-cart-shopping"></i>
-                                Cart
-                            </Link>
-                    </div>
-                ) : (
-                    <div className="navbar-container">
-                        <Link to="/home">
-                            <img src="gracebuster-logo.png" class="logo" />
-                        </Link>
-                        <div className="nav-right">
-                            <Link to="/shop">Shop</Link>
-                            <Link to="/login">Login</Link>
-                            <Link to="/signup">Sign Up</Link>
-                            <Link to="/cart">
-                                <i className="fa-solid fa-cart-shopping"></i>
-                                Cart
-                            </Link>
-                        </div>
-                    </div>
-                )}
-            </nav>
-            <hr />
-        </div>
-    )
-}
+  return (
+    <div>
+      <nav className="navbar">
+        {isLoggedIn ? (
+          <div className="navbar-container">
+            <Link to="/home">
+              <img src="gracebuster-logo.png" className="logo" />
+            </Link>
+            <Link to="/shop">
+              Shop <i className="fa-solid fa-cassette-tape"></i>
+            </Link>
+            <Link to="/account">Account</Link>
+            <button type="button" onClick={logoutAndRedirectHome}>
+              Logout
+            </button>
+            <Link to="/cart">
+              <i className="fa-solid fa-cart-shopping"></i>
+              Cart
+            </Link>
+          </div>
+        ) : (
+          <div className="navbar-container">
+            <Link to="/home">
+              <img src="gracebuster-logo.png" className="logo" />
+            </Link>
+            <div className="nav-right">
+              <Link to="/shop">Shop</Link>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Sign Up</Link>
+              <Link to="/cart">
+                <i className="fa-solid fa-cart-shopping"></i>
+                Cart
+              </Link>
+            </div>
+          </div>
+        )}
+      </nav>
+      <hr />
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
