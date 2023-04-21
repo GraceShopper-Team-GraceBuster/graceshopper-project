ab23f18502f655e6e1487939d3c43094e2c46fa8
Richard <Richman962@gmail.com>
2023-04-21 11:13:29 -0400

    Cart stuff

---
+++ server/db/models/CartItems.js
@@ -0,0 +1,19 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+const Movies = require("./Movies");
+
+const CartItems = db.define("CartItems", {
+  quantity: {
+    type: Sequelize.INTEGER,
+  },
+  MovieId: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    references: {
+      model: Movies,
+      key: "id",
+    },
+  },
+});
+
+module.exports = CartItems;
