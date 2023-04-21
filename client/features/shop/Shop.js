a1fefdbd15dca70884f3921bce199ba1d43a0240
abrahamflores <a.flrs182@gmail.com>
2023-04-21 09:26:55 -0700

    added styling to single movies page, formatting

--- client/features/shop/Shop.js
+++ client/features/shop/Shop.js
@@ -11,7 +11,6 @@ const Shop = () => {
           <h1>Browse our Titles!</h1>
           <p>search through many popular movies!</p>
         </div>
-        {/* do we want a search bar here or filter? */}
         <Movies />
       </div>
       <footer>
