9203f0c1b68a0dd8b9ebebf2681e3738724a8d5e
Justin Cappellini <cappellinij@gmail.com>
2023-04-26 01:39:27 +0800

    added render db,removed code from users. new admin

--- server/api/users.js
+++ server/api/users.js
@@ -2,37 +2,11 @@ const router = require('express').Router();
 const { models } = require('../db');
 const { User, Cart, Product } = models;
 
-// Middleware to authenticate users based on token in request headers
-async function authenticateUser(req, res, next) {
-  const authHeader = req.headers.authorization;
-  if (authHeader) {
-    const token = authHeader.split(' ')[1];
-    try {
-      const user = await User.findByToken(token);
-      req.user = user;
-    } catch (error) {
-      console.log(error);
-    }
-  }
-  next();
-}
-
-// Middleware to restrict access to admin users only
-function adminOnly(req, res, next) {
-  if (req.user && req.user.isAdmin) {
-    next();
-  } else {
-    res.sendStatus(403);
-  }
-}
-
-router.use(authenticateUser);
-
-// Get all users (admin only)
-router.get('/', adminOnly, async (req, res, next) => {
+// Get all users
+router.get('/', async (req, res, next) => {
   try {
     const users = await User.findAll({
-      attributes: ['id', 'username', 'email', 'isAdmin'],
+      attributes: ['id', 'username', 'email'],
     });
     res.json(users);
   } catch (err) {
@@ -40,11 +14,11 @@ router.get('/', adminOnly, async (req, res, next) => {
   }
 });
 
-// Get user by ID (admin only)
-router.get('/:id', adminOnly, async (req, res, next) => {
+// Get user by ID
+router.get('/:id', async (req, res, next) => {
   try {
     const user = await User.findByPk(req.params.id, {
-      attributes: ['id', 'username', 'email', 'isAdmin'],
+      attributes: ['id', 'username', 'email'],
       include: [{ model: Cart, include: [{ model: Product }] }],
     });
     if (!user) {
@@ -61,6 +35,8 @@ module.exports = router;
 
 
 
+
 // explicitly select only the id and username fields - even though
 // users' passwords are encrypted, it won't help if we just
 // send everything to anyone who asks!
+
